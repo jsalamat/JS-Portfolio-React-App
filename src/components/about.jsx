24990c65bd39ab3d9d0526e3f4f0a5f8de47180b
Jon Salamat <jsalamat@milestechnologies.com>
2024-08-18 14:48:58 -0400

    minor update to about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -16,7 +16,7 @@ const Aboutus  = () => {
             <div className="about-content">
                 <h2 className="title-about">About Jon</h2>
                 <p className="decs-about">Jon is a skilled Front-End Developer with 4+ years of experience, specializing in creating engaging user experiences. He excels in website development, feature optimization, and debugging, and is proficient in WordPress, HTML, PHP, CSS, and JavaScript. With a background in Graphic and Web Design, Jon brings a creative edge to his work. He&#x27;s passionate about delivering top-tier digital experiences and is excited to contribute his expertise to new projects and challenges. Let&#x27;s work together to bring your vision to life.</p>
-                <p className="decs-about">Let create something fun together</p>
+                <p className="decs-about about-cta">Let create something fun together!</p>
             </div>
         </div>
     </section>
